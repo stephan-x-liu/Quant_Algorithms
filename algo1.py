67c43bf3b8bdfb1a6c42601d50e79e5a3a67eb34
guardianxero <guardianxero@gmail.com>
2014-03-19 20:47:51 -0700

    first algorithm.

---
+++ algo1.py
@@ -0,0 +1,16 @@
+# Put any initialization logic here.  The context object will be passed to
+# the other methods in your algorithm.
+def initialize(context):
+    pass
+
+# Will be called on every trade event for the securities you specify. 
+def handle_data(context, data):
+    # Implement your algorithm logic here.
+
+    # data[sid(X)] holds the trade event data for that security.
+    # context.portfolio holds the current portfolio state.
+
+    # Place orders with the order(SID, amount) method.
+
+    # TODO: implement your own logic here.
+    order(sid(24), 50)
