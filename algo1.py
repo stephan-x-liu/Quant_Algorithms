115bf3834ebc0658034a2634a68647ca68371b0b
guardianxero <guardianxero@gmail.com>
2014-03-22 18:36:56 -0600

    Kunal sucks at copying and pasting

--- algo1.py
+++ algo1.py
@@ -1,21 +1,16 @@
-# Put any initialization logic here.  The context object will be passed to
-# the other methods in your algorithm.
-import math
-import numpy as np  
-
 atr  = ta.ATR(timeperiod = 7)
 bbands = ta.BBANDS(timeperiod = 7)
 calc_inital_trend=True
 high_close_SIC=0 
-low_close_SIC=Integer.MAX_VALUE
+low_close_SIC=99999999
 uptrend=True 
 
 def initialize(context):
-    ##Trade with stock spy
-    stock_id=8554  ## SPY stock Id, used for testing 
-    context.stock=stock_id
-    ## change the context.values?
-    set_universe(universe.DollarVolumeUniverse(floor_precentile=98.0,ceiling_percentile=100.0)) ## sets the universe to the current mode
+	##Trade with stock spy
+
+	context.stock=sid(8554)
+	## change the context.values?
+	set_universe(universe.DollarVolumeUniverse(floor_percentile=98.0,ceiling_percentile=100.0)) ## sets the universe to the current mode
 
 
 # Will be called on every trade event for the securities you specify. 
@@ -23,85 +18,82 @@ def initialize(context):
 # 
 def handle_data(context, data):
 
-	nonlocal calc_inital_trend 
-	nonlocal high_close_SIC
-	nonlocal low_close_SIC
-	nonlocal uptrend
+	global calc_inital_trend 
+	global high_close_SIC
+	global low_close_SIC
+	global uptrend
 
 	# money=context.portfolio.MONEY
 
 	already_bought=False
-    # Implement your algorithm logic here.
+	# Implement your algorithm logic here.
 
-    ##Find value of volatility stop 
+	##Find value of volatility stop 
 
-    
+	
 
-    ## calculating bbands 
-    bbands_data = bbands(data) # returns a tuple of (upper, line, lower)
-    time_period=7
+	## calculating bbands 
 
-    upper_band=bbands_data[0]
-    lower_band=bbands_data[2]
 
-    
-    # data[sid(X)] holds the trade event data for that security.
-    # context.portfolio holds the current portfolio state.
+	
+	# data[sid(X)] holds the trade event data for that security.
+	# context.portfolio holds the current portfolio state.
 
-    # Place orders with the order(SID, amount) method.
+	# Place orders with the order(SID, amount) method.
 
-    # TODO: implement your own logic here.
+	# TODO: implement your own logic here.
 
-    #this will only order if a volatility boundary is hit 
+	#this will only order if a volatility boundary is hit 
 
-    volatility=volatility_stop(context,data)
+	volatility=volatility_stop(context,data)
 
-    if uptrend and not already_bought:
-    	order(sid(context.stock_id), 50)
-    	already_bought=True 
+	if uptrend and not already_bought:
+		order(context.stock, 50)
+		already_bought=True 
 
-    if uptrend:
-    	if data[stock_id].price< volatility:
-    		order(sid(context.stock_id), -50)
-    		uptrend=False
+	if uptrend:
+		if data[context.stock].price< volatility:
+			order(context.stock, -50)
+			uptrend=False
 
    	if not uptrend:
-   		if data[stock_id].price>volatility:
-   			order(sid(context.stock_id), 50)
+   		if data[context.stock].price>volatility:
+   			order(context.stock, 50)
    			uptrend=True
 
 
 def volatility_stop(context,data):  #measures volatility stop for 7 day period
 	
-	nonlocal calc_inital_trend
-	nonlocal high_close_SIC
-	nonlocal low_close_SIC
-	nonlocal uptrend
+	global calc_inital_trend 
+	global high_close_SIC
+	global low_close_SIC
+	global uptrend
+
 
 	if calc_inital_trend:
 		uptrend=(history(1,"1d",'close_price')- history(7,"1d",'open_price'))>0
 		calc_inital_trend=False
 
 	# Below calculate the SIC 
-	i=1
-	price_history=[]
+	k=(1,2,3,4,5,6,7)
+	i=0
 
-	while i<8:
-		price_history.append(history(bar_count=i, frequency="1d", field='close_price')) #tuple with closing price i days ago
 
+	price_history = history(bar_count=7, frequency="1d", field='close_price')[context.stock] #tuple with closing price i days ago
+     
+        
 	if uptrend:
-		high_close_SIC=max(price_history)
+		high_close_SIC=max(high_close_SIC,max(price_history))
 	else:
-		low_close_SIC=min(price_history)
+		low_close_SIC=min(low_close_SIC,min(price_history))
 
-    atr_data = atr(data) # initaliaze the data
-    atr_value=atr_data[context.stock]
-    atr_value*=3 	
+	atr_data = atr(data) # initaliaze the data
+	atr_value=atr_data[context.stock]
+	atr_value*=3 	
 
 	 ## returns tuple with boolean true for uptrend and the highclose or low close
 	if uptrend:
 		return high_close_SIC-atr_value
 	return low_close_SIC+atr_value
-
 
 
